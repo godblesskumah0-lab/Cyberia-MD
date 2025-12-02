009d5792028f61f2198a1311b66fda32bcad17ae
godblesskumah0-lab <godblesskumah0@gmail.com>
2025-12-02 14:11:07 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " ",  // add your Session Id make sure it starts with 
-  OWNER_NUMBER: "27649342626", // PUT OWNER NUMBER HERE
+  SESSION_ID: "suho~nFNiFaaB#1p5bYnrV9NNwAU8eqbxcf2PDmo1Fva5CjEN3gfRVaao ",  // add your Session Id make sure it starts with 
+  OWNER_NUMBER: "233534793257", // PUT OWNER NUMBER HERE
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
